8d6af7cc4ce9aa643e05b5e40c6a51a102498083
Isaac Hsu <16hsuisaac@gmail.com>
2021-07-12 16:52:32 -0700

    updates image src on input for photo URL, also changed font to sans-serif

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 /* global data */
 /* exported data */
+
+var photoURL = document.querySelector('.photourl');
+var photo = document.querySelector('img');
+
+photoURL.addEventListener('input', updateImage);
+
+function updateImage(event) {
+  photo.setAttribute('src', photoURL.value);
+}

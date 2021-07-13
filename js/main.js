590fc00519f0db287ab8e8bfba4a4a5d71d531b3
Isaac Hsu <16hsuisaac@gmail.com>
2021-07-13 14:57:05 -0700

    fixed function call and used loop to call journal entries

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var photo = document.querySelector('img');
 var form = document.querySelector('.form');
 var title = document.querySelector('.title');
 var notes = document.querySelector('.notes');
+var ul = document.querySelector('ul');
 
 photoURL.addEventListener('input', updateImage);
 form.addEventListener('submit', submit);
@@ -23,3 +24,34 @@ function submit(event) {
   photo.setAttribute('src', 'images/placeholder-image-square.jpg');
   form.reset();
 }
+
+function journalView(entry) {
+  var li = document.createElement('li');
+  li.setAttribute('class', 'row');
+
+  var img = document.createElement('img');
+  img.setAttribute('class', 'column-half');
+  img.setAttribute('src', entry.url);
+  li.appendChild(img);
+  var div = document.createElement('div');
+  div.setAttribute('class', 'column-half');
+  li.appendChild(div);
+  var h2 = document.createElement('h2');
+  div.appendChild(h2);
+  var entryTitle = document.createTextNode(entry.title);
+  h2.append(entryTitle);
+  var p = document.createElement('p');
+  div.appendChild(p);
+  var description = document.createTextNode(entry.notes);
+  p.append(description);
+
+  return li;
+}
+
+function parentJournalView(event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    ul.appendChild(journalView(data.entries[i]));
+  }
+}
+
+window.addEventListener('DOMContentLoaded', parentJournalView);

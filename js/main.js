e9582a3288e5baeae9a9ad922d70a23162bad749
Isaac Hsu <16hsuisaac@gmail.com>
2021-07-12 17:11:37 -0700

    added nextEntryID to object and increments it, prepends new object to the entries in the data model, reset input models

--- js/main.js
+++ js/main.js
@@ -3,9 +3,24 @@
 
 var photoURL = document.querySelector('.photourl');
 var photo = document.querySelector('img');
+var form = document.querySelector('.form');
+var title = document.querySelector('.title');
+var notes = document.querySelector('.notes');
+var i = 1;
 
 photoURL.addEventListener('input', updateImage);
+form.addEventListener('submit', submit);
 
 function updateImage(event) {
   photo.setAttribute('src', photoURL.value);
+}
+
+function submit(event) {
+  var object = { Title: title.value, URL: photoURL.value, Notes: notes.value };
+  object.nextEntryID = i;
+  i++;
+  data.entries.append(object);
+  photoURL.value = 'images/placeholder-image-square.jpg';
+  title.value = '';
+  notes.value = '';
 }
